11569158d7d5334c954e8ab0362ebf1803702d2a
Kush <tanishqs1403@gmail.com>
2025-04-12 22:29:28 +0530

    fixing /

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const app = express();
 const prismaClient = new PrismaClient();
 
 app.use(cors({
-    origin: "https://uptime-checker-py3w.vercel.app/",
+    origin: "https://uptime-checker-py3w.vercel.app",
 }))
 app.use(express.json());
 
