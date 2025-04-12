12cfb4597f7474de3cef7975e56dd2bf57a07cac
Kush <tanishqs1403@gmail.com>
2025-04-12 22:22:24 +0530

    adding clerk auth

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,8 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction){
-    const authorizationToken = req.headers["authorization"]
-    // Verify the JWT
-    req.userId = "1";
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(" ")[1];
+    if (!token) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+    let decoded
+    decoded = jwt.decode(token, process.env.JWT_SECRET as string, { algorithms: ['RS256'] });
+    // console.log(decoded);
+    if(!decoded?.sub){
+        res.json({error:"Unauthorized"});
+        return;
+    }
+    req.userId = decoded?.sub;
     next();
-}
+};
