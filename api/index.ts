b64a4062454bdaa4ed8710e503e3cd28e45f2412
Kush <tanishqs1403@gmail.com>
2025-04-13 00:56:39 +0530

    fixing cors again

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ const prismaClient = new PrismaClient();
 
 app.use(
     cors({
-      origin: ['https://uptime-checker-py3w.vercel.app','http://localhost:3000'], 
+      origin: ['https://web-monitor-frontend.vercel.app/dashboard','http://localhost:3000'], 
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
